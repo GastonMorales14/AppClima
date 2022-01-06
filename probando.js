1ab06b6c1e0854a0c10d8a725672208f799123f9
BernardoLarsen <bernardolar99@gmail.com>
2022-01-06 17:25:43 -0300

    aceptoCambios

---
+++ probando.js
@@ -0,0 +1,4 @@
+"use script";
+const API_URL = 'http://api.weatherunlocked.com/api/current/51.50,-0.12?app_id=14b371e8&app_key=cbbb5e4cf852450e95c9bdcf9fc562cb';
+let variable = document.getElementById('#h2').value;
+console.log(variable);
